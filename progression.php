085f42363e3496b1992306af36c358055f617d27
Mykolaichuk <bogmw.it@gmail.com>
2016-11-02 14:29:39 +0200

    some fix

--- progression.php
+++ progression.php
@@ -5,6 +5,15 @@ $end = $_POST['progEnd'];
 $step = $_POST['progStep'];
 $sum = 0;
 
+if($step <= 0) {
+    echo "Крок не може бути меншич чи дорівнювати 0";
+    return;
+}
+if($start > $end) {
+    echo "Початкове значення не може бути більше ніж кінцеве";
+    return;
+}
+
 function progression($start, $end, $step){
     if ($start < $end+1) {
         echo "$start ";
